6113dc2caa8e3ce30892cd6a97209dfe536d3dc9
StacyFranco <stacyfranco@hotmail.com>
2024-03-18 21:50:31 -0300

    Clear code, and adding bases for cadastro e login

---
+++ src/services/RegisterServices.ts
@@ -0,0 +1,7 @@
+import { HttpApiServices } from "./HttpApiServices";
+
+export class RegisterServices extends HttpApiServices {
+    register(body:any){
+        return this.post('/auth/register', body);
+    }
+}
---
+++ src/views/Login.tsx
@@ -0,0 +1,66 @@
+import {useState} from 'react';
+import logo from '../assets/images/logo.svg';
+import loginIcon from '../assets/images/mail.svg';
+import passwordIcon from '../assets/images/key.svg';
+import { PublicInput } from '../components/general/PublicInput';
+import { LoginServices } from '../services/LoginServices';
+import { Link, useSearchParams } from 'react-router-dom';
+
+const loginServices = new LoginServices();
+
+export const Login = () => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const [searchParams] = useSearchParams();
+    const success = searchParams.get('success');
+
+    const doLogin = async() => {
+        try{
+            setError('');
+            if(!login || login.trim().length < 5
+                || !password || password.trim().length < 4){
+                return setError('Favor preencher os campos corretamente.');
+            }
+
+            setLoading(true);
+            await loginServices.login({login, password});
+            setLoading(false);
+        }catch(e:any){
+            console.log('Erro ao efetuar login:', e);
+            setLoading(false);
+            if(e?.response?.data?.message){
+                return setError(e?.response?.data?.message);
+            }
+            return setError('Erro ao efetuar login, tente novamente');
+        }
+    }
+ 
+    return (
+        <div className="container-public">
+            <img src={logo} alt="Logo devameet" className='logo'/>
+            <form>
+                {error && <p className='error'>{error}</p>}
+                {success && <p className='success'>Cadastro efetuado com sucesso, faça seu login.</p>}
+
+                <PublicInput icon={loginIcon} alt="Email" name="Email"
+                    type="text" modelValue={login} setValue={setLogin}/>
+                
+                <PublicInput icon={passwordIcon} alt="Senha" name="Senha"
+                    type="password" modelValue={password} setValue={setPassword}/>
+
+                <button type='button' onClick={doLogin} disabled={loading}>
+                    {loading ? '...Carregando': 'Login'}
+                </button>
+
+                <div className='link'>
+                    <p>Não possui uma conta?</p>
+                    <Link to='/register'>Faça seu cadastro agora!</Link>
+                </div>
+            </form>
+        </div>
+    );
+}
